8b893ea342282e433147f1b7c0299bd4474fa255
sp-95 <shashankap95@gmail.com>
2023-04-12 21:31:42 +0000

    Deploying to gh-pages from @ UMD-2022/shatoru-lifecycle@9ef5798992069b3f384cd18f1538de568257cf56 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.post1.dev1+g126e06e',
+    VERSION: '0.0.post1.dev1+g9ef5798',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
